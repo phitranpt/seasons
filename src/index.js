636c252d4aac1e6ef73464fe4d554d927ddb5281
phitranpt <phi.tran.pt@gmail.com>
2020-03-21 13:53:03 -0500

    add conditional rendering

--- src/index.js
+++ src/index.js
@@ -6,20 +6,33 @@ import ReactDOM from 'react-dom';
         super(props);
 
         //THIS IS THE ONLY TIME we do direct assignment to this.state
-        this.state = { lat: null }
+        this.state = { 
+            lat: null,
+            errorMessage: '',
+        };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({ lat: position.coords.latitude });
             },
-            err => console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
         );
 
     }
 
     //React says we have to define render!!
      render() {
-        return <div>Latitude: {this.state.lat}</div>
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>
      }
  }
 
