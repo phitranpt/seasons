49d6ff1b9106c52bcc0cfcb92d33ff327f71f2aa
phitranpt <phi.tran.pt@gmail.com>
2020-03-21 13:08:54 -0500

    added setState for latitude

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
 
  class App extends React.Component {
-     render() {
+    constructor(props) {
+        super(props);
+
+        //THIS IS THE ONLY TIME we do direct assignment to this.state
+        this.state = { lat: null }
+
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
             err => console.log(err)
         );
 
-         return <div>Latitude:</div>
+    }
+
+    //React says we have to define render!!
+     render() {
+        return <div>Latitude: {this.state.lat}</div>
      }
  }
 
