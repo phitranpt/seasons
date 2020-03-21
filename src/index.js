06702ad6fc0026df0a240cf6349b67c0ae29ef6f
phitranpt <phi.tran.pt@gmail.com>
2020-03-21 14:18:25 -0500

    alternate state initialization using babel

--- src/index.js
+++ src/index.js
@@ -2,24 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
  class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = {
+        lat: null,
+        errorMessage: ''
+    };
 
-        //THIS IS THE ONLY TIME we do direct assignment to this.state
-        this.state = { 
-            lat: null,
-            errorMessage: '',
-        };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
-
     }
 
     //React says we have to define render!!
