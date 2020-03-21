7b527614e8bf0ac6e04109cebffbd357f538b571
phitranpt <phi.tran.pt@gmail.com>
2020-03-21 12:30:03 -0500

    second commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonDisplay = () => {
+    return (
+        <div>
+            Season Display
+        </div>
+    );
+ };
+
+ export default SeasonDisplay
